080f326de815105b5d18f7f611704fe01a3e5fc9
Sandy-233 <93172297+Sandy-233@users.noreply.github.com>
2026-06-26 20:56:07 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,1 +1,47 @@
+const QUESTIONS = [
 
+{
+id:1,
+
+subject:"Pathology",
+
+chapter:"Cell Injury",
+
+question:"Which cytokine is the strongest stimulator of fibrosis?",
+
+options:[
+"IL-1",
+"IFN-γ",
+"TNF-α",
+"TGF-β"
+],
+
+correct:3,
+
+explanation:"TGF-β is the most potent profibrotic cytokine."
+
+},
+
+{
+id:2,
+
+subject:"Medicine",
+
+chapter:"Cardiology",
+
+question:"Normal adult pulse rate?",
+
+options:[
+"40/min",
+"72/min",
+"120/min",
+"150/min"
+],
+
+correct:1,
+
+explanation:"Normal adult pulse is 60–100/min."
+
+}
+
+];
